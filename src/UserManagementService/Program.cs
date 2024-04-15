40e6b4a3e1c38ef3e34f99c1fc135cda926e2db9
Renato Moraes Pinheiro <renatomoraespinheiro@gmail.com>
2024-04-14 23:32:07 -0300

    feat: docker compose working for both services

--- src/UserManagementService/Program.cs
+++ src/UserManagementService/Program.cs
@@ -5,7 +5,6 @@ using UserManagementService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -16,11 +15,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Urls.Add("http://0.0.0.0:80");
 
 app.Run();
